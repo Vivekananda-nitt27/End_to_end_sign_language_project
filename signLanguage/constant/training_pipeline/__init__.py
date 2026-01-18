b6f5c0b9f4302c0ac93711d609b895ec92d6c02c
vivekananda <vivekanandasahoo34@gmail.com>
2026-01-18 19:22:16 +0530

    The data valid comit

--- signLanguage/constant/training_pipeline/__init__.py
+++ signLanguage/constant/training_pipeline/__init__.py
@@ -15,3 +15,16 @@ DATA_DOWNLOAD_URL: str = (
     "https://github.com/Vivekananda-nitt27/End_to_end_sign_language_project/raw/main/data/sign_language_data.zip"
 )
 
+"""
+Data Validation related constants start with DATA_VALIDATION_ variable name
+"""
+
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+
+DATA_VALIDATION_STATUS_FILE: str = "status.txt"
+
+DATA_VALIDATION_ALL_REQUIRED_FILES = [
+    "train",
+    "test",
+    "data.yaml"
+]
