028a8330c8eeeed7e4b45a9640cdf8277004220f
vivekananda <vivekanandasahoo34@gmail.com>
2026-01-18 16:48:31 +0530

    The data upload

---
+++ signLanguage/entity/config_entity.py
@@ -0,0 +1,33 @@
+import os
+from dataclasses import dataclass
+from datetime import datetime
+
+from signLanguage.constant.training_pipeline import *
+
+
+# Timestamp for artifacts
+TIMESTAMP: str = datetime.now().strftime("%m_%d_%Y_%H_%M_%S")
+
+
+@dataclass
+class TrainingPipelineConfig:
+    artifacts_dir: str = os.path.join(ARTIFACTS_DIR, TIMESTAMP)
+
+
+# Create a single instance to reuse across configs
+training_pipeline_config: TrainingPipelineConfig = TrainingPipelineConfig()
+
+
+@dataclass
+class DataIngestionConfig:
+    data_ingestion_dir: str = os.path.join(
+        training_pipeline_config.artifacts_dir,
+        DATA_INGESTION_DIR_NAME
+    )
+
+    feature_store_file_path: str = os.path.join(
+        data_ingestion_dir,
+        DATA_INGESTION_FEATURE_STORE_DIR
+    )
+
+    data_download_url: str = DATA_DOWNLOAD_URL
